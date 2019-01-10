5447eec72d401820fb7e2e915cc787dcf2c3d09e
Desdemo <guodesdemo@gmail.com>
2019-01-10 11:23:17 +0800

    校验

--- photo/Pupoe_photo.py
+++ photo/Pupoe_photo.py
@@ -17,26 +17,36 @@ for i in b:
   b = "./{0}/".format(a)
   base_path = os.listdir(b)
 
+  print("---------------------------------------")
+
   for department in base_path:
     d = list(os.listdir(b + "{0}/".format(department)))  #获取部门
+
+    print("部门：" + department)
+
 
     for j in d:
       path = ("./" + base_src + "/" + a + "/" + "{0}/{1}".format(department, j))  # 获取图片路径
       name = os.path.splitext(j)[0]  # 获取姓名
-      photo_data = {}
-      photo_data["name"] = name
-      photo_data["avatar"] = path
-      photo_data["data"] = {}
-      photo_data["data"]["department"] = department
 
-      #  photo_data = json.JSONEncoder(ensure_ascii=False).encode(photo_data)
-      datas.append(photo_data)
-      random.shuffle(datas)  # 生成随机列表
-    print(datas)
-    with open('../json_data/{0}.json'.format(a), 'w') as f:  # 写入JSON文件
-      json.dump(datas, f)
-      # json.dump(datas, f, ensure_ascii=False)
+      print(name)
 
+    print("---------------------------------------")
+      # print(name + department)
+    #   photo_data = {}
+    #   photo_data["name"] = name
+    #   photo_data["avatar"] = path
+    #   photo_data["data"] = {}
+    #   photo_data["data"]["department"] = department
+    #
+    #   #  photo_data = json.JSONEncoder(ensure_ascii=False).encode(photo_data)
+    #   datas.append(photo_data)
+    #   random.shuffle(datas)  # 生成随机列表
+    # print(datas)
+    # with open('../json_data/{0}.json'.format(a), 'w') as f:  # 写入JSON文件
+    #   json.dump(datas, f)
+    #   # json.dump(datas, f, ensure_ascii=False)
+    #
 
 
 
